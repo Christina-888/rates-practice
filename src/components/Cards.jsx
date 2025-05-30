c3fc6bceb2106585863bf348f3ce86437ed10441
Christina-888 <christinabykadorova@gmail.com>
2025-05-30 12:16:40 +0300

    add modules

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,12 @@
-import './cards.css'
+import styles from './cards.module.css'
 
 const Cards = ({ title, subtitle, speed, condition, titleColor, subtitleColor }) => {
   return (
-    <div className='card'>
-      <h1 className={`card__title ${titleColor}`}>{title}</h1>
-      <h2 className={`card__subtitle ${subtitleColor}`}>{subtitle}</h2>
-      <p className='card__paragraph'>{speed}</p>
-      <p className='card__paragraph card__paragraph_second'>{condition}</p>
+    <div className={styles.card}>
+      <h1 className={`${styles.card__title} ${styles[titleColor]}`}>{title}</h1>
+      <h2 className={`${styles.card__subtitle} ${styles[subtitleColor]}`}>{subtitle}</h2>
+      <p className={styles.card__paragraph}>{speed}</p>
+      <p className={`${styles.card__paragraph} ${styles.card__paragraph_second}`}>{condition}</p>
     </div>
   )
 }
@@ -17,62 +17,62 @@ const CardItems = () => {
       "title": "Безлимитный 300",
       "subtitle": (
         <>
-          <sup>руб.</sup><span className="subtitle-number">300</span><sub>/мес.</sub>
+          <sup>руб.</sup><span className={styles.subtitleNumber}>300</span><sub>/мес.</sub>
         </>
       ),
       "speed": "до 10 Мбит/сек.",
       "condition": (
         <span>Объём включённого <br></br> трафика не ограничен</span>
       ),
-      "titleColor": "color-darkblue",
-      "subtitleColor": "color-blue",
+      "titleColor": "colorDarkblue",
+      "subtitleColor": "colorBlue",
     },
     {
       "title": "Безлимитный 450",
       "subtitle": (
         <>
-          <sup>руб.</sup><span className="subtitle-number">450</span><sub>/мес.</sub>
+          <sup>руб.</sup><span className={styles.subtitleNumber}>450</span><sub>/мес.</sub>
         </>
       ),
       "speed": "до 50 Мбит/сек.",
       "condition": (
         <span>Объём включённого <br></br> трафика не ограничен</span>
       ),
-      "titleColor": "color-darkgreen",
-      "subtitleColor": "color-green",
+      "titleColor": "colorDarkgreen",
+      "subtitleColor": "colorGreen",
     },
     {
       "title": "Безлимитный 550",
       "subtitle": (
         <>
-          <sup>руб.</sup><span className="subtitle-number">550</span><sub>/мес.</sub>
+          <sup>руб.</sup><span className={styles.subtitleNumber}>550</span><sub>/мес.</sub>
         </>
       ),
       "speed": "до 100 Мбит/сек.",
       "condition": (
         <span>Объём включённого <br></br> трафика не ограничен</span>
       ),
-      "titleColor": "color-darkred",
-      "subtitleColor": "color-red",
+      "titleColor": "colorDarkred",
+      "subtitleColor": "colorRed",
     },
     {
       "title": "Безлимитный 1000",
       "subtitle": (
         <>
-          <sup>руб.</sup><span className="subtitle-number">1000</span><sub>/мес.</sub>
+          <sup>руб.</sup><span className={styles.subtitleNumber}>1000</span><sub>/мес.</sub>
         </>
       ),
       "speed": "до 200 Мбит/сек.",
       "condition": (
         <span>Объём включённого <br></br> трафика не ограничен</span>
       ),
-      "titleColor": "color-black",
-      "subtitleColor": "color-lightblack",
+      "titleColor": "colorBlack",
+      "subtitleColor": "colorLightblack",
     }
   ]
 
   return (
-    <div className='container'>
+    <div className={styles.container}>
       {items.map((item) => (
         <Cards
         title={item.title}
